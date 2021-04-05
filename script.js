d1be374eb203b7903a18de28ebc552745d3cc600
Curtis Laurence Chadwell <>
2021-04-05 00:49:59 +0000

    Added alt way of converting a object to an array

--- script.js
+++ script.js
@@ -15,11 +15,19 @@ const obj = {'first': 1, 'second': 2, 'third': 3}
 
 //new way of converting a object to an array
 const newOToA = (obj) =>{
-  const array = Object.entries(obj)
+  let array = Object.entries(obj)
   
   return array
 }
 
 console.log(console.log(newOToA(obj)))
+//or
 
+//This is considering that the key is a string...if the key was a number on line 28 change string to number
+const altOToA = (obj) =>{
+  let array = Object.keys(obj).map(key => [String(key), obj[key]])
 
+  return array
+}
+
+console.log(altOToA(obj))
